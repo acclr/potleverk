34b0cd74681f6f2083c4825fb398844f840686f0
Kim <76793289+kimskogsmo@users.noreply.github.com>
2026-04-11 18:29:45 +0200

    Implement generateMetadata for page metadata retrieval
    Add generateMetadata function to fetch page metadata

--- app/[uid]/page.tsx
+++ app/[uid]/page.tsx
@@ -2,6 +2,18 @@ import { getPage } from "@/features/prismic/actions/getPage";
 import { components } from "@/features/prismic/slices";
 import { SliceZone } from "@prismicio/react";
 
+export const generateMetadata = async ({params}: { params: Promise<{ uid: string }> }) => {
+  const { uid } = (await params);
+  const prismicPage = await getPage(uid).catch(err => {
+    console.error(err);
+    return null;
+  });
+  return {
+    title: prismicPage?.data.meta_title,
+    description: prismicPage?.data.meta_description,
+  }
+}
+
 export default async function Page({params}: { params: Promise<{ uid: string }> }) {
   const { uid } = (await params);
   const prismicPage = await getPage(uid)?.catch((err) => {
